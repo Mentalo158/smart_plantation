4a25c81d0e7a4a133ad7b1a4cf80d184488be614
Mentalo158 <stewartbenyamin12345@gmail.com>
2024-10-12 17:02:27 +0200

    You can read adcvalues in get % out

--- project/Smart_Plantation/main/main.c
+++ project/Smart_Plantation/main/main.c
@@ -3,6 +3,14 @@
 #include "nvs_flash.h"
 #include "esp_err.h"
 #include "wifi-server.h"
+#include "adc_sensor.c"
+
+#define ADC_CH_4 ADC1_CHANNEL_4 // GPIO 32 auf dem ESP
+#define ADC_CH_5 ADC1_CHANNEL_5 // GPIO 33 auf dem ESP
+
+#define ADC_BITWIDTH ADC_WIDTH_BIT_12 //Je höher desto genauer hat seine vor- und nachteile zu lang zum erklären.
+// Wenn die Bit anzahl verändert wird muss auch die adcToPercentage funktion angepasst werden adc durch max Bitbreite adc/2^12 - 1 bspw.
+
 void app_main(void)
 {
     // Keine Ahnung, ChatGPT sagt das ist wichtig und cool
@@ -15,4 +23,8 @@ void app_main(void)
 
     wifi_connection();
     start_webserver();
+
+    // adc_init(ADC_CH_4, ADC_BITWIDTH); // Muss vorher einmal aktiviert werden zum konfigurieren des jeweiligen channels
+    // float adcPercentage = adc_read_sensor(ADC_CH_4); // ein Sensor einlesen, einfach channel übergeben implementier das in ner while schleife
+    // adc_cleanup();      // Um die ADC's auszuschalten am Ende wenn wir das benötigen(Nicht in while nur eimal ausführen)
 }
