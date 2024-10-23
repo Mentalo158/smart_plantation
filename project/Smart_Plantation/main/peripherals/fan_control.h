8dffd6dc90abe1112cfeacdae4709fcb7b3a5e8f
Stewart Benyamin <stewart.benyamin@student.fh-kiel.de>
2024-10-23 08:39:35 +0200

    fixed

--- project/Smart_Plantation/main/peripherals/fan_control.h
+++ project/Smart_Plantation/main/peripherals/fan_control.h
@@ -1,34 +1,27 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#ifndef FAN_CONTROL_H
+#define FAN_CONTROL_H
+
+#include <stdint.h>
 #include "driver/ledc.h"
-#include "esp_err.h"
-#include "fan_control.h"
 
-void fan_init(int pinFan)
-{
-    ledc_timer_config_t ledc_timer = {
-        .speed_mode = LEDC_MODE,
-        .timer_num = LEDC_TIMER_1,           
-        .duty_resolution = LEDC_FAN_DUTY_RES, 
-        .freq_hz = LEDC_FAN_FREQUENCY,        
-        .clk_cfg = LEDC_AUTO_CLK             
-    };
-    ledc_timer_config(&ledc_timer);
+// LEDC Konfiguration für den Lüfter
+#define LEDC_CHANNEL_FAN LEDC_CHANNEL_3
+#define LEDC_FAN_DUTY_RES LEDC_TIMER_8_BIT // 8 Bit Auflösung für den Lüfter
+#define LEDC_FAN_DUTY_MAX 255              // Maximaler Duty-Cycle (100%)
+#define LEDC_FAN_FREQUENCY 25000           // 25 kHz für den Lüfter
 
-    ledc_channel_config_t ledc_channel_fan = {
-        .speed_mode = LEDC_MODE,
-        .channel = LEDC_CHANNEL_FAN, 
-        .timer_sel = LEDC_TIMER_1,   
-        .intr_type = LEDC_INTR_DISABLE,
-        .gpio_num = pinFan, 
-        .duty = 0,          
-        .hpoint = 0};
-    ledc_channel_config(&ledc_channel_fan);
-}
+/**
+ * @brief Initialisiert die Lüftersteuerung.
+ *
+ * @param pinFan Der GPIO-Pin, der mit dem PWM-Eingang des Lüfters verbunden ist.
+ */
+void fan_init(int pinFan);
 
-void fan_set_speed(uint8_t speed)
-{
-    ledc_set_duty(LEDC_MODE, LEDC_CHANNEL_FAN, speed);
-    ledc_update_duty(LEDC_MODE, LEDC_CHANNEL_FAN);
-}
+/**
+ * @brief Setzt die Lüftergeschwindigkeit.
+ *
+ * @param speed Lüftergeschwindigkeit (0 bis 255).
+ */
+void fan_set_speed(uint8_t speed);
+
+#endif // FAN_CONTROL_H
