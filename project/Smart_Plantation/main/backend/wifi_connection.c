1ce5adf2aa1168aa3960bfca89fc27b8605eaa6c
Mentalo158 <stewartbenyamin12345@gmail.com>
2024-12-03 00:40:40 +0100

    kommentare angepasst

--- project/Smart_Plantation/main/backend/wifi_connection.c
+++ project/Smart_Plantation/main/backend/wifi_connection.c
@@ -37,33 +37,30 @@ void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, in
 
 void wifi_connection()
 {
-    esp_netif_init();                    // Initialisiere das Netzwerkinterface
-    esp_event_loop_create_default();     // Erstelle die Standard-Ereignisschleife
-    esp_netif_create_default_wifi_sta(); // Erstelle ein Standard-WLAN-Station-Interface
+    esp_netif_init();                    
+    esp_event_loop_create_default();     
+    esp_netif_create_default_wifi_sta(); 
 
-    // WLAN-Initialisierung
+    
     wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
     esp_wifi_init(&wifi_initiation);
 
-    // Registriere die Ereignis-Handler
+    
     esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
     esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
 
-    // WLAN-Konfiguration
     wifi_config_t wifi_configuration = {
         .sta = {
             .ssid = CONFIG_WIFI_SSID,
             .password = CONFIG_WIFI_PASSWORD,
         }};
 
-    // SSID und Passwort setzen
     strcpy((char *)wifi_configuration.sta.ssid, ssid);
     strcpy((char *)wifi_configuration.sta.password, pass);
 
-    // Konfiguration anwenden
     esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
-    esp_wifi_start();                 // WLAN starten
-    esp_wifi_set_mode(WIFI_MODE_STA); // Setze den WLAN-Modus auf Station
-    esp_wifi_connect();               // Verbinde mit dem WLAN
+    esp_wifi_start();                 
+    esp_wifi_set_mode(WIFI_MODE_STA); 
+    esp_wifi_connect();              
     printf("wifi_init_softap finished. SSID:%s  password:%s", ssid, pass);
 }
