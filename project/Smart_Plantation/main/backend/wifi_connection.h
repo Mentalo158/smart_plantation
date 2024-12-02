aec22982be2e0fa479f95af0e2849fb5a9ef2d7f
Mentalo158 <stewartbenyamin12345@gmail.com>
2024-12-02 19:20:03 +0100

    vars angepasst

--- project/Smart_Plantation/main/backend/wifi_connection.h
+++ project/Smart_Plantation/main/backend/wifi_connection.h
@@ -10,11 +10,6 @@
 #include "sdkconfig.h"
 #include "sntp_client.h"
 
-// Globale Variablen
-extern const char *ssid;
-extern const char *pass;
-extern int retry_num;
-
 // Funktionen
 void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
 void wifi_connection();
